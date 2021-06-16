995aac58da2ee58581b76fe8f603f55fa41c8807
alishahzil <alishahzil@gmail.com>
2021-06-16 21:47:57 +0500

    ali shahzil

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SlimLoadingBarModule} from 'ng2-slim-loading-bar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MemberAddComponent } from './member-add/member-add.component';
+import { MemberGetComponent } from './member-get/member-get.component';
+import { MemberEditComponent } from './member-edit/member-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MemberAddComponent,
+    MemberGetComponent,
+    MemberEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SlimLoadingBarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
